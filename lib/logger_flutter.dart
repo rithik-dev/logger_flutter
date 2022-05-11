b88f8110fa6d475f674f6ef985ceb06f17c73bb6
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-05-12 02:38:24 +0530

    feat: added files

--- lib/logger_flutter.dart
+++ lib/logger_flutter.dart
@@ -1,7 +1,5 @@
+// @github/rithik-dev
+//
 library logger_flutter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/logger.dart';
